5e055cfd3606521152c691851a1ca4d87e75b105
andresfequinteropa-arch <andresfe.quinteropa@ecci.edu.co>
2026-07-02 20:21:42 -0500

    Update test_fallo.py

--- test_fallo.py
+++ test_fallo.py
@@ -5,7 +5,7 @@ def dividir(a, b):
     return a / b
 
 def test_multiplicacion():
-    assert multiplicar(3, 4) == 12  # ❌ 3x4 = 12, no 15
+    assert multiplicar(3, 4) == 14  # ❌ 3x4 = 12, no 15
 
 def test_division():
-    assert dividir(10, 2) == 5  # ❌ 10/2 = 5, no 6
+    assert dividir(10, 2) == 8  # ❌ 10/2 = 5, no 6
